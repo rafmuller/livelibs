ae591b33eb5d3be16ec4beb74d8ea4c454d9367d
Rafael Muller <rmuller@cisco.com>
2019-05-22 15:17:21 -0400

    More cleanup of the code

--- tenant/js/aci/apic.js
+++ tenant/js/aci/apic.js
@@ -1,18 +1,18 @@
 function classQuery(classname) {
 	return $.ajax({
-		url     : 'https://' + $.cookie('apic_ip') + '/api/node/class/' + classname + '.json',
-		headers : {
-			DevCookie        : $.cookie('token'),
-			'APIC-challenge' : $.cookie('urlToken'),
-			'Content-Type'   : 'application/json'
+		url: 'https://' + $.cookie('apic_ip') + '/api/node/class/' + classname + '.json',
+		headers: {
+			DevCookie: $.cookie('token'),
+			'APIC-challenge': $.cookie('urlToken'),
+			'Content-Type': 'application/json'
 		}
 	});
 }
 
 function buildTableData(aci_endpoint_data) {
 	table_data = [];
-	$.each(aci_endpoint_data['imdata'], function(i, endpoint) {
-		row = [ endpoint.fvCEp.attributes.mac, endpoint.fvCEp.attributes.ip ];
+	$.each(aci_endpoint_data['imdata'], function (i, endpoint) {
+		row = [endpoint.fvCEp.attributes.mac, endpoint.fvCEp.attributes.ip];
 		table_data.push(row);
 	});
 	return table_data;
@@ -20,21 +20,20 @@ function buildTableData(aci_endpoint_data) {
 
 function get_fabric_node_data() {
 	return $.ajax({
-		url     :
-			'https://' +
+		url: 'https://' +
 			$.cookie('apic_ip') +
 			'/api/node/class/dhcpClient.json?query-target-filter=and(not(wcard(dhcpClient.dn,%22__ui_%22)),and(and(ne(dhcpClient.nodeId,"0"),ne(dhcpClient.ip,"0.0.0.0")),or(eq(dhcpClient.nodeRole,"spine"),eq(dhcpClient.nodeRole,"leaf"))))',
-		headers : {
-			DevCookie        : $.cookie('token'),
-			'APIC-challenge' : $.cookie('urlToken'),
-			'Content-Type'   : 'application/json'
+		headers: {
+			DevCookie: $.cookie('token'),
+			'APIC-challenge': $.cookie('urlToken'),
+			'Content-Type': 'application/json'
 		}
 	});
 }
 
 function proc_fabric_node_data(fabric_data) {
 	table_data = [];
-	$.each(fabric_data['imdata'], function(i, device) {
+	$.each(fabric_data['imdata'], function (i, device) {
 		if (device.dhcpClient.attributes.clientEvent == 'assigned') {
 			table_row = [
 				device.dhcpClient.attributes.name,
@@ -51,11 +50,11 @@ function proc_fabric_node_data(fabric_data) {
 
 function get_fabric_health_data() {
 	return $.ajax({
-		url     : 'https://' + $.cookie('apic_ip') + '/api/node/mo/topology/HDfabricOverallHealth5min-0.json',
-		headers : {
-			DevCookie        : $.cookie('token'),
-			'APIC-challenge' : $.cookie('urlToken'),
-			'Content-Type'   : 'application/json'
+		url: 'https://' + $.cookie('apic_ip') + '/api/node/mo/topology/HDfabricOverallHealth5min-0.json',
+		headers: {
+			DevCookie: $.cookie('token'),
+			'APIC-challenge': $.cookie('urlToken'),
+			'Content-Type': 'application/json'
 		}
 	});
 }
--- tenant/js/aci/apic_auth.js
+++ tenant/js/aci/apic_auth.js
@@ -1,12 +1,12 @@
 function apic_get_token() {
 	return $.ajax({
-		type : 'POST',
-		url  : 'https://' + $.cookie('apic_ip') + '/api/aaaLogin.json?gui-token-request=yes',
-		data : JSON.stringify({
-			aaaUser : {
-				attributes : {
-					name : $.cookie('apic_user'),
-					pwd  : $.cookie('apic_pass')
+		type: 'POST',
+		url: 'https://' + $.cookie('apic_ip') + '/api/aaaLogin.json?gui-token-request=yes',
+		data: JSON.stringify({
+			aaaUser: {
+				attributes: {
+					name: $.cookie('apic_user'),
+					pwd: $.cookie('apic_pass')
 				}
 			}
 		})
@@ -14,9 +14,18 @@ function apic_get_token() {
 }
 
 function set_apic_cookies(data) {
-	$.cookie('havetoken', 'Y', { path: '/', expires: 1 });
-	$.cookie('token', data['imdata'][0]['aaaLogin']['attributes']['token'], { path: '/', expires: 1 });
-	$.cookie('urlToken', data['imdata'][0]['aaaLogin']['attributes']['urlToken'], { path: '/', expires: 1 });
+	$.cookie('havetoken', 'Y', {
+		path: '/',
+		expires: 1
+	});
+	$.cookie('token', data['imdata'][0]['aaaLogin']['attributes']['token'], {
+		path: '/',
+		expires: 1
+	});
+	$.cookie('urlToken', data['imdata'][0]['aaaLogin']['attributes']['urlToken'], {
+		path: '/',
+		expires: 1
+	});
 }
 
 function logout_apic() {
