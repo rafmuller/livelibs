7ddba2a5ca92994ec372aa9b605ea4b0c0681485
Rafael Muller <rmuller@cisco.com>
2023-04-10 15:05:24 -0400

    Updates to dashboard part of the lab tenant app

--- tenant/js/aci/apic.js
+++ tenant/js/aci/apic.js
@@ -41,16 +41,14 @@ function get_fabric_node_data() {
 function proc_fabric_node_data(fabric_data) {
 	table_data = [];
 	$.each(fabric_data['imdata'], function (i, device) {
-		if (device.dhcpClient.attributes.clientEvent == 'assigned') {
-			table_row = [
-				device.dhcpClient.attributes.name,
-				device.dhcpClient.attributes.id,
-				device.dhcpClient.attributes.nodeId,
-				device.dhcpClient.attributes.model,
-				device.dhcpClient.attributes.runningVer
-			];
-			table_data.push(table_row);
-		}
+		table_row = [
+			device.dhcpClient.attributes.name,
+			device.dhcpClient.attributes.id,
+			device.dhcpClient.attributes.nodeId,
+			device.dhcpClient.attributes.model,
+			device.dhcpClient.attributes.runningVer
+		];
+		table_data.push(table_row);
 	});
 	return table_data;
 }
@@ -73,7 +71,7 @@ function proc_fabric_leaf_list(fabric_data) {
 
 function get_fabric_health_data() {
 	return $.ajax({
-		url: 'http://' + $.cookie('apic_ip') + '/api/node/mo/topology/HDfabricOverallHealth5min-0.json',
+		url: 'http://' + $.cookie('apic_ip') + '/api/node/mo/topology/health.json',
 		headers: {
 			DevCookie: $.cookie('token'),
 			'APIC-challenge': $.cookie('urlToken'),
@@ -83,5 +81,48 @@ function get_fabric_health_data() {
 }
 
 function get_fabric_health(health_data) {
-	return health_data.imdata[0].fabricOverallHealthHist5min.attributes.healthAvg;
+	return health_data.imdata[0].fabricHealthTotal.attributes.cur;
+}
+
+function get_fabric_fault_data() {
+	return $.ajax({
+		url: 'http://' + $.cookie('apic_ip') + '/api/class/faultCountsWithDetails.json',
+		headers: {
+			DevCookie: $.cookie('token'),
+			'APIC-challenge': $.cookie('urlToken'),
+			'Content-Type': 'application/json'
+		}
+	});
+}
+
+function process_fabric_fault_data(fabric_fault_data) {
+	fault_obj = {
+		"crit": fabric_fault_data["imdata"][0]["faultCountsWithDetails"]["attributes"]["crit"],
+		"maj": fabric_fault_data["imdata"][0]["faultCountsWithDetails"]["attributes"]["maj"],
+		"min": fabric_fault_data["imdata"][0]["faultCountsWithDetails"]["attributes"]["minor"],
+		"warn": fabric_fault_data["imdata"][0]["faultCountsWithDetails"]["attributes"]["warn"]
+	}
+	return fault_obj;
+}
+
+function get_fabric_tenants() {
+	return $.ajax({
+		url: 'http://' + $.cookie('apic_ip') + '/api/class/fvTenant.json',
+		headers: {
+			DevCookie: $.cookie('token'),
+			'APIC-challenge': $.cookie('urlToken'),
+			'Content-Type': 'application/json'
+		}
+	});
+}
+
+function get_fabric_dashenpoints() {
+	return $.ajax({
+		url: 'http://' + $.cookie('apic_ip') + '/api/class/fvCEp.json',
+		headers: {
+			DevCookie: $.cookie('token'),
+			'APIC-challenge': $.cookie('urlToken'),
+			'Content-Type': 'application/json'
+		}
+	});
 }
